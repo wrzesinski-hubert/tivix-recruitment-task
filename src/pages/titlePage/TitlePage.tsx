9e203890f96e7a97cf8fb1a247aa5adfa5ad496d
wrzesinski-hubert <wrzesinski.hubert@gmail.com>
2023-10-12 20:33:58 +0200

    create title page

---
+++ src/pages/titlePage/TitlePage.tsx
@@ -0,0 +1,11 @@
+import { Title } from "./styles";
+
+function TitlePage() {
+  return (
+    <div>
+      <Title> Lego minifigs mystery box</Title>
+    </div>
+  );
+}
+
+export default TitlePage;
---
+++ src/pages/titlePage/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-family: Roboto, sans-serif;
+  font-size: 1.5em;
+  text-align: center;
+  color: #bf4f74;
+  text-transform: uppercase;
+`;
