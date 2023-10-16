d38211dc35f4a39447adadadbe5b424661d4b94b
wrzesinski-hubert <wrzesinski.hubert@gmail.com>
2023-10-16 02:17:22 +0200

    refactor fetching data from fetching by set to fetching by theme

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,11 +1,3 @@
-export type MinifigType = {
-  id: number;
-  quantity: number;
-  set_img_url: string;
-  set_name: string;
-  set_num: string;
-};
-
 export type MinifigWithDetailType = {
   name: string;
   set_img_url: string;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-import { MinifigType, MinifigWithDetailType } from "../types/types";
+import { MinifigWithDetailType } from "../types/types";
 import { API_KEY } from "./config";
 
 async function fetchAPI(url: string) {
@@ -19,79 +19,55 @@ async function fetchAPI(url: string) {
   }
 }
 
-async function fetchRandomSet(setName: string) {
-  const url = `https://rebrickable.com/api/v3/lego/sets/?min_parts=1&search=${setName}`;
-  const data = await fetchAPI(url);
-  const shuffledSets = data.results?.sort(() => 0.5 - Math.random());
-  return shuffledSets[0];
+async function repeatFetch(url: string, additionalParameter?: string) {
+  const data = await fetchAPI(
+    url + "?page_size=1" + (additionalParameter ? additionalParameter : "")
+  );
+  const count = data.count;
+
+  const urlWithCount =
+    url +
+    `?page_size=${count}` +
+    (additionalParameter ? additionalParameter : "");
+  const data2 = await fetchAPI(urlWithCount);
+
+  return data2;
 }
 
-const fetchedSetNumbers = new Set();
-
-async function getMinifigsFromSet(setNum: string, allMinifigs: MinifigType[]) {
-  //Don't fetch Minifigs if already fetched
-  if (fetchedSetNumbers.has(setNum)) {
-    return [];
-  }
-  fetchedSetNumbers.add(setNum);
-  const url = `https://rebrickable.com/api/v3/lego/sets/${setNum}/minifigs/`;
-  const data = await fetchAPI(url);
-  const allFigsFromSet = data.results;
-
-  // Filter out minifigs that are already in allMinifigs because some set has same Minifigs
-  const newMinifigs = allFigsFromSet.filter(
-    (fig: { id: number }) =>
-      !allMinifigs.some(
-        (existingFig: { id: number }) => existingFig.id === fig.id
-      )
+async function fetchAllThemes(setName: string) {
+  const data2 = await repeatFetch(
+    "https://rebrickable.com/api/v3/lego/themes/"
   );
 
-  return newMinifigs;
+  const allThemes = data2.results.filter(
+    (theme: { id: number; parent_id: number; name: string }) =>
+      theme.name.includes(setName)
+  );
+  return allThemes;
 }
 
-async function fetchDetailsAboutFigure(setNum: string) {
-  const url = `https://rebrickable.com/api/v3/lego/minifigs/${setNum}`;
-  const details = await fetchAPI(url);
-  return details;
+async function fetchMinifigsFromTheme(
+  allThemes: { id: number; parent_id: number; name: string }[]
+) {
+  let allMiniFigs: any[] = [];
+  const fetchPromises = allThemes.map(async (theme) => {
+    const data2 = await repeatFetch(
+      "https://rebrickable.com/api/v3/lego/minifigs/",
+      `&in_theme_id=${theme.id}`
+    );
+    allMiniFigs = allMiniFigs.concat(data2.results);
+  });
+
+  await Promise.all(fetchPromises);
+  return allMiniFigs;
 }
 
 export async function fetchRandomMinifigs(amountOfFigures: number) {
-  const fetchAllMinifigs = async (amountOfFigures: number) => {
-    let allMinifigs: MinifigType[] = [];
-
-    while (allMinifigs.length < amountOfFigures) {
-      const randomSet = await fetchRandomSet("Harry Potter");
-      const minifigsFromSet = await getMinifigsFromSet(
-        randomSet.set_num,
-        allMinifigs
-      );
-      allMinifigs = allMinifigs.concat(minifigsFromSet);
-      if (allMinifigs.length < amountOfFigures) {
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-      }
-    }
-
-    return allMinifigs;
-  };
-  const allMinifigs = await fetchAllMinifigs(amountOfFigures);
-
-  const fetchDetailsAboutArrayOfFigures = async (
-    randomFigures: MinifigType[]
-  ) => {
-    return Promise.all(
-      randomFigures.map(async (figure) => {
-        const figureWithDetail = await fetchDetailsAboutFigure(figure.set_num);
-        return figureWithDetail;
-      })
-    );
-  };
-
-  const shuffled = allMinifigs.sort(() => 0.5 - Math.random());
+  const allThemes = await fetchAllThemes("Harry Potter");
+  const allMiniFigs = await fetchMinifigsFromTheme(allThemes);
+  const shuffled = allMiniFigs.sort(() => 0.5 - Math.random());
   const randomFigures = shuffled.slice(0, amountOfFigures);
-  const randomFiguresWithDetails = await fetchDetailsAboutArrayOfFigures(
-    randomFigures
-  );
-  return randomFiguresWithDetails;
+  return randomFigures;
 }
 
 export async function getPartsOfMinifig(setNum?: string) {
